c145d91c4a76b3bd5a8daff54338f6162a084d47
IsukapalliSivaRamaDeekshithulu <sivaramlucky451@gmail.com>
2024-04-13 17:42:04 +0530

    first commit

---
+++ dataStructuresAndAlgorithms/src/binaryTreeNode/BinaryTreeNode.java
@@ -0,0 +1,41 @@
+package binaryTreeNode;
+
+public class BinaryTreeNode {
+
+	// variables
+	public int data;
+	public BinaryTreeNode left, right;
+
+	// Constructer
+	BinaryTreeNode(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	// getter and setter methods
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+
+}
---
+++ dataStructuresAndAlgorithms/src/insertAndDeleteOperations/BinaryTreeInsertAndDelete.java
@@ -0,0 +1,18 @@
+package insertAndDeleteOperations;
+
+import binaryTreeNode.BinaryTreeNode;
+
+public class BinaryTreeInsertAndDelete {
+
+	static void insertValue(int value,BinaryTreeNode bt) {
+		if(bt==null) {
+			
+		}
+		
+	}
+	
+	static void deleteValue(int value,BinaryTreeNode bt) {
+		
+	}
+
+}
